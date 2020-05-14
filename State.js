62c45a905a736df1e6b073843ed8b52ca56ef367
Sophonias Isaac <sophonias8@gmail.com>
2020-05-14 14:51:02 +0100

    Update State.js

--- State.js
+++ State.js
@@ -1,1 +1,36 @@
 
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button>Change!</button>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+//useState 
+
+
+function App(){
+    const [count ]= useState(0)
+    return(
+        <div>
+        <h1>{count} </h1>
+        <button></button>
+        </div>
+    )
+}
