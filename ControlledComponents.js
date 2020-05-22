4414a25e1f179db92d82f78876e2f762c7be5c7c
Sophonias Isaac <sophonias8@gmail.com>
2020-05-22 13:31:35 +0100

    Create ControlledComponents.js
    controlled components with hooks

---
+++ ControlledComponents.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [inputfield, setInput] = useState('')
+
+   const updateInput = (event) =>{
+   
+    setInput(event.target.value)
+  }
+
+    return (
+   
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value={inputfield} onChange={updateInput}/>
+<h1>{inputfield}</h1>
+          
+        </div>
+      </div>
+    );
+  }
+
+
+export default App;
