b957034399a6e1ac4b87e48232915b3d3065d6e1
Sophonias Isaac <sophonias8@gmail.com>
2020-05-22 13:37:30 +0100

    Update ControlledComponents.js

--- ControlledComponents.js
+++ ControlledComponents.js
@@ -28,3 +28,35 @@ function App() {
 
 
 export default App;
+
+
+
+
+//slightly different way. 
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [inputfield, setInput] = useState('')
+
+
+
+    return (
+   
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value={inputfield} onChange={(e) => setInput(e.target.value)}/>
+          <p>{inputfield}</p>
+          
+        </div>
+      </div>
+    );
+  }
+
+
+export default App;
